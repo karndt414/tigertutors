9a137b4fc7456c94558cee8852ce448fe81329c6
karndt414 <koree.arndt@gmail.com>
2025-11-30 10:03:00 -0600

    Test

--- bhstigertutors-react/src/pages/HomePage.jsx
+++ bhstigertutors-react/src/pages/HomePage.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 
 function HomePage() {
     return (
-        <div>
-            <h2>Welcome to BHS Tiger Tutors!</h2>
-            <p style={{ lineHeight: 1.7, fontSize: '1.1em' }}>
-                We are the Bentonville High School chapter of Mu Alpha Theta, the National Mathematics Honor Society.
-                Our mission is to provide high-quality, free peer tutoring to BHS students across a variety of subjects.
-            </p>
-            <p style={{ lineHeight: 1.7, fontSize: '1.1em', marginTop: '1em' }}>
-                Use this site to find a qualified tutor for your class, learn more about our chapter, or get in touch with our officers.
-            </p>
+        <div style={{ padding: '50px', border: '2px solid red' }}>
+            <h1>HELLO, THIS IS THE HOME PAGE</h1>
         </div>
     );
 }
