e76109bffad22fc65007286188c8cdcc8d8185e7
karndt414 <koree.arndt@gmail.com>
2025-11-30 09:33:01 -0600

    Remove Calendy link

--- bhstigertutors-react/src/AdminPanel.jsx
+++ bhstigertutors-react/src/AdminPanel.jsx
@@ -7,7 +7,6 @@ function AdminPanel({ tutors, onTutorAdded, onSignOut }) {
     const [name, setName] = useState('');
     const [subjects, setSubjects] = useState('');
     const [photo, setPhoto] = useState('');
-    const [bookingLink, setBookingLink] = useState('');
     const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
@@ -26,7 +25,6 @@ function AdminPanel({ tutors, onTutorAdded, onSignOut }) {
             setName('');
             setSubjects('');
             setPhoto('');
-            setBookingLink('');
             // Trigger a re-fetch of the tutor list in App.jsx
             onTutorAdded();
         }
@@ -80,13 +78,6 @@ function AdminPanel({ tutors, onTutorAdded, onSignOut }) {
                     placeholder="Photo URL"
                     value={photo}
                     onChange={(e) => setPhoto(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Calendly Booking Link"
-                    value={bookingLink}
-                    onChange={(e) => setBookingLink(e.target.value)}
-                    required
                 />
                 <button type="submit" disabled={loading}>
                     {loading ? 'Adding...' : 'Add Tutor'}
