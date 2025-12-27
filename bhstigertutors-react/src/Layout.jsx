fb0b85b85cd665aeed5053598b28b178542b19dd
karndt414 <koree.arndt@gmail.com>
2025-12-26 23:47:42 -0600

    Fix layout

--- bhstigertutors-react/src/Layout.jsx
+++ bhstigertutors-react/src/Layout.jsx
@@ -6,23 +6,23 @@ function Layout({ openLogin }) {
     <div className="App"> {/* Changed to 'App' to match most standard CSS setups */}
       <nav className="navbar">
         <div className="nav-container">
-          <Link to="/" className="logo">BHS Tutors</Link>
+          <Link to="/" className="logo">Tiger Tutors</Link>
           <div className="nav-links">
             <Link to="/">Home</Link>
-            <Link to="/tutors">Find a Tutor</Link>
-            <Link to="/group-tutoring">Group Sessions</Link>
-            <button onClick={openLogin} className="login-button">Admin</button>
+            <Link to="/about">About</Link>
+            <Link to="/tutors">Tutors</Link>
+            <Link to="/sessions">Sessions</Link>
           </div>
+          <button onClick={openLogin} className="login-button">Login</button>
         </div>
       </nav>
 
-      <main className="main-content">
+      <div className="main-content">
         <Outlet />
-      </main>
+      </div>
 
       <footer className="footer">
-        <p>© 2025 BHS Tiger Tutors</p>
-        <button onClick={openLogin} className="footer-link">Staff Login</button>
+        <p>&copy; 2025 Tiger Tutors. All rights reserved.</p>
       </footer>
     </div>
   );
