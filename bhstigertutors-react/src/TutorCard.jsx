d9cb59cbc57dcd210be629d2784cfbfa40f26bcf
karndt414 <koree.arndt@gmail.com>
2025-12-28 02:54:44 -0600

    Tutor card update

--- bhstigertutors-react/src/TutorCard.jsx
+++ bhstigertutors-react/src/TutorCard.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './TutorCard.css';
 
-// This component takes 'props' (properties) and displays them.
 function TutorCard({ name, subjects, photo, bookingLink }) {
     return (
         <div className="tutor-card">
-            <img src={photo} alt={`Profile of ${name}`} />
-            <h3>{name}</h3>
-            <p>{subjects}</p>
-            <a href={bookingLink} className="book-button" target="_blank" rel="noopener noreferrer">
-                Book a Session
-            </a>
+            <div className="tutor-photo">
+                <img src={photo} alt={`Profile of ${name}`} />
+            </div>
+            <div className="tutor-info">
+                <h3>{name}</h3>
+                <p>{subjects}</p>
+                <a href={bookingLink} className="book-button" target="_blank" rel="noopener noreferrer">
+                    Book a Session
+                </a>
+            </div>
         </div>
     );
 }
