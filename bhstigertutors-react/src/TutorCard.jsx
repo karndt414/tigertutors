66c65974436930ac32a45a60edbd02fd915640e1
karndt414 <koree.arndt@gmail.com>
2026-01-25 19:46:13 -0600

    image fix

--- bhstigertutors-react/src/TutorCard.jsx
+++ bhstigertutors-react/src/TutorCard.jsx
@@ -4,10 +4,14 @@ import './TutorCard.css';
 function TutorCard({ name, subjects, photo }) {
     return (
         <div className="tutor-card">
-            <img src={photo} alt={name} />
-            <div className="tutor-card-content">
-                <h3>{name}</h3>
-                <p>{subjects}</p>
+            <div className="tutor-photo">
+                <img src={photo} alt={name} />
+            </div>
+            <div className="tutor-info">
+                <div>
+                    <h3>{name}</h3>
+                    <p>{subjects}</p>
+                </div>
             </div>
         </div>
     );
